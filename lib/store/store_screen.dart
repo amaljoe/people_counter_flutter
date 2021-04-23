62737e0275410c0f892ed2c3aba2dae3b5801a60
Amal Joe R S <amaljoe88@gmail.com>
2021-04-23 16:31:58 +0530

    add initial 10 results

--- lib/store/store_screen.dart
+++ lib/store/store_screen.dart
@@ -23,66 +23,98 @@ class _StoreScreenState extends State<StoreScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          appBar: AppBar(
-            elevation: 0,
-            backgroundColor: Colors.transparent,
-            leading: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.arrow_back,
-                color: Colors.black,
-              ),
-            ),
-            title: Text(
-              'Margin Free',
-              style: TextStyle(
-                fontSize: 24,
-                color: Colors.black,
-                fontWeight: FontWeight.w600,
-              ),
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.black,
             ),
           ),
-          body: StreamBuilder<QuerySnapshot>(
-            stream: _firestore
-                .collection('activity')
-                .orderBy('time', descending: true)
-                .limit(1)
-                .snapshots(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return Container(
-                  child: Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                );
-              }
-              final activity = snapshot.data.docs.first;
-              bool contains = false;
-              listItems.forEach((element) {
-                if (element.id == activity.id) {
-                  contains = true;
-                  return;
+          title: Text(
+            'Margin Free',
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        body: FutureBuilder(
+          future: _firestore
+              .collection('activity')
+              .orderBy('time', descending: true)
+              .limit(10)
+              .get(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text("Something went wrong");
+            }
+
+            if (snapshot.connectionState == ConnectionState.done) {
+              List<QueryDocumentSnapshot> docs = snapshot.data.docs;
+              docs.forEach((doc) {
+                bool contains = false;
+                listItems.forEach((element) {
+                  if (element.id == doc.id) {
+                    contains = true;
+                    return;
+                  }
+                });
+                if (!contains) {
+                  listItems.add(Activity.fromSnapshot(doc));
                 }
               });
-              if (!contains) {
-                listItems.insert(0, Activity.fromSnapshot(activity));
-                if (_listKey.currentState != null) {
-                  _listKey.currentState.insertItem(0);
-                }
-              }
-              return AnimatedList(
-                key: _listKey,
-                physics: BouncingScrollPhysics(),
-                initialItemCount: listItems.length,
-                padding: EdgeInsets.all(8),
-                itemBuilder: (context, index, animation) {
-                  return FadeTransition(
-                      opacity: animation,
-                      child: ActivityItem(listItems[index]));
+              return StreamBuilder<QuerySnapshot>(
+                stream: _firestore
+                    .collection('activity')
+                    .orderBy('time', descending: true)
+                    .limit(1)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Container(
+                      child: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    );
+                  }
+                  final activity = snapshot.data.docs.first;
+                  bool contains = false;
+                  listItems.forEach((element) {
+                    if (element.id == activity.id) {
+                      contains = true;
+                      return;
+                    }
+                  });
+                  if (!contains) {
+                    listItems.insert(0, Activity.fromSnapshot(activity));
+                    if (_listKey.currentState != null) {
+                      _listKey.currentState.insertItem(0);
+                    }
+                  }
+                  return AnimatedList(
+                    key: _listKey,
+                    physics: BouncingScrollPhysics(),
+                    initialItemCount: listItems.length,
+                    padding: EdgeInsets.all(8),
+                    itemBuilder: (context, index, animation) {
+                      return FadeTransition(
+                          opacity: animation,
+                          child: ActivityItem(listItems[index]));
+                    },
+                  );
                 },
               );
-            },
-          )),
+            }
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ),
+      ),
     );
   }
 }
